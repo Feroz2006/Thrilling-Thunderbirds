9950dcff16fc34dae0b29c8a9142f514e7f3a00f
Crispin <crispinr003@gmail.com>
2022-07-26 01:46:26 +0530

    feat: update all the routes

--- src/frontend/src/config/routes.js
+++ src/frontend/src/config/routes.js
@@ -1,17 +1,38 @@
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Play from "../pages/Play";
+import MazeGame from "../pages/MazeGame";
+import SnakeGame from "../pages/SnakeGame";
 
 const routes = [
   {
     path: "",
     component: Home,
     name: "Home Page",
-    protected: true,
+    protected: false,
   },
   {
     path: "/login",
     component: Login,
     name: "Login Screen",
+    protected: false,
+  },
+  {
+    path: "/play",
+    component: Play,
+    name: "Play Page",
+    protected: false,
+  },
+  {
+    path: "/play/maze",
+    component: MazeGame,
+    name: "Maze Game Page",
+    protected: false,
+  },
+  {
+    path: "/play/snake",
+    component: SnakeGame,
+    name: "Snake Game Page",
     protected: false,
   },
 ];
