584e6d53253ac8299329ac17ee7961d86536495a
Crispin <crispinr003@gmail.com>
2022-07-25 12:54:11 +0530

    chore: update file organization

--- src/frontend/src/pages/Home.js
+++ src/frontend/src/pages/Home.js
@@ -1,10 +1,7 @@
-import { useEffect } from "react";
 import Logout from "../components/auth/Logout";
 import Center from "../components/utils/Center";
 
 const Home = (props) => {
-  useEffect(() => {}, []); // eslint-disable-line react-hooks/exhaustive-deps
-
   return (
     <Center>
       <Logout />
