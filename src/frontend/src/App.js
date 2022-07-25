8b5384fc8a5fe4853574f588e3de4d6198ae35dc
Crispin <crispinr003@gmail.com>
2022-07-26 01:46:54 +0530

    feat: initialize tsparticles in app

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,13 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { CircularProgress } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { auth } from "./config/firebase";
 import routes from "./config/routes";
 import Center from "./components/utils/Center";
 import AuthChecker from "./components/auth/AuthChecker";
+import "./assets/scss/style.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import particlesConfig from "./components/utils/particlesjs-config.json";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
+
+  const theme = createTheme({
+    typography: {
+      fontFamily: "Primer Print, serif",
+      button: {
+        textTransform: "none",
+      },
+    },
+  });
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -28,27 +45,30 @@ function App() {
     );
 
   return (
-    <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                route.protected ? (
-                  <AuthChecker>
+    <>
+      <ThemeProvider theme={theme}>
+        <Particles options={particlesConfig} init={particlesInit} />
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Routes>
+            {routes.map((route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  route.protected ? (
+                    <AuthChecker>
+                      <route.component />
+                    </AuthChecker>
+                  ) : (
                     <route.component />
-                  </AuthChecker>
-                ) : (
-                  <route.component />
-                )
-              }
-            />
-          ))}
-        </Routes>
-      </BrowserRouter>
-    </div>
+                  )
+                }
+              />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
