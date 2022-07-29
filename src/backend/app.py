cabc4546d50d5eb682ae53a3abef6486c77db247
RyugaXhypeR <ryugashyper@gmail.com>
2022-07-29 17:02:09 +0530

    Passed pre-commit

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,37 +1,35 @@
 from importlib import import_module
 from pathlib import Path
 
-from fastapi import FastAPI
-from pymongo import MongoClient
-
 from constants import Database
+from fastapi import FastAPI
 from log import get_logger
-
+from pymongo import MongoClient
 
 log = get_logger(__name__, 'setup')
 
 
 class RouterNotFound(Exception):
-    """ Raised when a router instance is not found in a module in `routers` dir"""
-    
+    """Raised when a router instance is not found in a module in `routers` dir"""
+
     def __init__(self, module: str) -> None:
         super().__init__(f'Router instance not found in {module}')
 
 
 class App(FastAPI):
-    """ Subclass for `FastAPI` to load the routers from `routers` folder."""
-    
+    """Subclass for `FastAPI` to load the routers from `routers` folder."""
+
     def __init__(self) -> None:
         super().__init__()
-    
+
     def setup(self) -> None:
-        """ Loading routers from `routers` directory """
+        """Loading routers from `routers` directory"""
         super().setup()
         routers = map(
             lambda x: str(x)[:-3].replace('\\', '.'),
             Path('routers').glob('**/[!_]*.py')
         )
-        
+
         for router_module in routers:
             module = import_module(router_module)
             try:
--- src/backend/log.py
+++ src/backend/log.py
@@ -4,15 +4,21 @@ from typing import Literal
 
 
 class UnknownLogMode(Exception):
-    """ Raised when unknown log mode is passed """
+    """Raised when unknown log mode is passed"""
+
     def __init__(self, mode: str) -> None:
         super().__init__(f'Excepted anyone of `{", ".join(LOG_FILE_MODES)}`, received `{mode}`')
 
 
-LOG_FILE_MODES = {'connection': 'connection.log', 'setup': 'setup.log', 'game': 'game_info.log'}
-
-DEFAULT_FORMAT = logging.Formatter(fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s', datefmt='%Y-%b-%d %H:%M:%S')
-
+LOG_FILE_MODES = {
+    'connection': 'connection.log',
+    'setup': 'setup.log',
+    'game': 'game_info.log'
+}
+DEFAULT_FORMAT = logging.Formatter(
+    fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+    datefmt='%Y-%b-%d %H:%M:%S'
+)
 DEFAULT_LOG_LEVEL = logging.DEBUG
 
 
@@ -24,7 +30,7 @@ def get_logger(
 ) -> logging.Logger:
     """
     Base logger for the application.
-    
+
     Attributes
     ----------
     name: str
@@ -34,17 +40,17 @@ def get_logger(
     log_format: Formatter
         Optional, default is `DEFAULT_FORMAT`
         the log format
-        
+
     Returns
     -------
     Logger
         The logger instance
-        
+
     Raises
     ------
     UnknownLogMode
         Raised when an unknown mode is passed
-        
+
     Example
     -------
     >>> log = get_logger(__name__, 'setup')
@@ -56,9 +62,9 @@ def get_logger(
         raise UnknownLogMode(mode) from None
     handler = logging.FileHandler(file)
     handler.setFormatter(log_format)
-    
+
     log = logging.getLogger(name)
     log.level = DEFAULT_LOG_LEVEL
     log.addHandler(handler)
-    
+
     return log
