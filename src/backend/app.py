52ded3b385a529ca7e5c9d9221addcd620927e77
RyugaXhypeR <ryugashyper@gmail.com>
2022-07-29 16:38:08 +0530

    base app

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,21 +1,44 @@
-import os
-import pprint
+from importlib import import_module
+from pathlib import Path
 
-from dotenv import load_dotenv
+from fastapi import FastAPI
 from pymongo import MongoClient
 
-load_dotenv()
+from constants import Database
+from log import get_logger
 
-client = MongoClient(os.getenv('MONGODB_URI'))
 
-# Database connection
-try:
-    print(client.server_info())
-    print("-----------Connected to MongoDB------------")
-except Exception:
-    print("Unable to connect to the server.")
+log = get_logger(__name__, 'setup')
 
-# Get sample data from the database
-db = client.get_database("sample_mflix")
-collection = db.get_collection('movies')
-pprint.pprint(collection.find_one())
+
+class RouterNotFound(Exception):
+    """ Raised when a router instance is not found in a module in `routers` dir"""
+    
+    def __init__(self, module: str) -> None:
+        super().__init__(f'Router instance not found in {module}')
+
+
+class App(FastAPI):
+    """ Subclass for `FastAPI` to load the routers from `routers` folder."""
+    
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def setup(self) -> None:
+        """ Loading routers from `routers` directory """
+        super().setup()
+        routers = map(
+            lambda x: str(x)[:-3].replace('\\', '.'),
+            Path('routers').glob('**/[!_]*.py')
+        )
+        
+        for router_module in routers:
+            module = import_module(router_module)
+            try:
+                self.include_router(module.router)
+                log.info(f'{module.__name__} loaded!')
+            except AttributeError:
+                raise RouterNotFound(router_module)
+
+
+database_client = MongoClient(Database.uri)
