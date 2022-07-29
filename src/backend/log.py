662b30f49e560550f7dd9876b88d1f84e86effae
RyugaXhypeR <ryugashyper@gmail.com>
2022-07-29 16:46:46 +0530

    Added docs
    Added doc strings for `get_logger` and `UnknownLogMode`
    and rearranged code

--- src/backend/log.py
+++ src/backend/log.py
@@ -1,9 +1,10 @@
 from logging import DEBUG, FileHandler, Formatter, getLogger, Logger
 from pathlib import Path
-from typing import Literal, Optional
+from typing import Literal
 
 
 class UnknownLogMode(Exception):
+    """ Raised when unknown log mode is passed """
     def __init__(self, mode: str) -> None:
         super().__init__(f'Excepted anyone of `{", ".join(LOG_FILE_MODES)}`, received `{mode}`')
 
@@ -15,26 +16,49 @@ DEFAULT_FORMAT = Formatter(fmt='%(asctime)s | %(name)s | %(levelname)s | %(messa
 DEFAULT_LOG_LEVEL = DEBUG
 
 
-class Handler(FileHandler):
-    def setFormatter(self, formatter: Optional[Formatter] = None):
-        super().setFormatter(formatter)
-
-
 def get_logger(
     name: str,
     mode: Literal['connection', 'setup', 'game'],
     *,
     log_format: Formatter = DEFAULT_FORMAT
 ) -> Logger:
+    """
+    Base logger for the application.
+    
+    Attributes
+    ----------
+    name: str
+        Name of the logger, usually `__name__`
+    mode: Literal['connection', 'setup', 'game']
+        Mode of the logger to log to a specific file
+    log_format: Formatter
+        Optional, default is `DEFAULT_FORMAT`
+        the log format
+        
+    Returns
+    -------
+    Logger
+        The logger instance
+        
+    Raises
+    ------
+    UnknownLogMode
+        Raised when an unknown mode is passed
+        
+    Example
+    -------
+    >>> log = get_logger(__name__, 'setup')
+    >>> log.info('Hello world!')
+    """
     try:
         file = Path('logs', LOG_FILE_MODES[mode])
     except KeyError:
         raise UnknownLogMode(mode) from None
-    
-    _log = getLogger(name)
-    _log.level = DEFAULT_LOG_LEVEL
-    handler = Handler(file)
+    handler = FileHandler(file)
     handler.setFormatter(log_format)
-    _log.addHandler(handler)
     
-    return _log
+    log = getLogger(name)
+    log.level = DEFAULT_LOG_LEVEL
+    log.addHandler(handler)
+    
+    return log
