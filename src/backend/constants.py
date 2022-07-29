f852a0ad6187a0a97c6f543606ac9b10de5ce85c
RyugaXhypeR <ryugashyper@gmail.com>
2022-07-29 22:21:32 +0530

    Switched to .env

--- src/backend/constants.py
+++ src/backend/constants.py
@@ -1,58 +1,13 @@
-from typing import Any
+from os import getenv
 
-from yaml import safe_load
+from dotenv import load_dotenv
 
+load_dotenv()
 
-with open('config.yaml') as file:
-    DATA = safe_load(file)
-
-
-class Undefined:
-    def __repr__(self) -> str:
-        return 'Undefined'
-    
-    def __bool__(self) -> False:
-        return False
+env = getenv('ENV')
 
 
-class NonConvertableType(Exception):
-    pass
+class Database:
+    """To store constants for database connection."""
 
-
-class YamlToClassAttr(type):
-    """
-    Tries to fetch data from config.yaml and making it class attr
-    Defaults to `Undefined` if the attr name is not found.
-    """
-    
-    def __getattr__(cls, attr: str) -> Any:
-        class_name = cls.__name__
-        
-        try:
-            cls_data = DATA[class_name]
-        except KeyError:
-            print(f'{class_name} not found in `config.yaml`')
-            return
-        
-        if attr not in cls.__dict__:
-            value = cls_data.get(attr, Undefined())
-            obj_type = cls.__annotations__[attr]
-            value_type = type(value)
-            # trying to convert the object to the type-hint
-            if value_type != obj_type:
-                
-                try:
-                    value = obj_type(value)
-                except (TypeError, ValueError):
-                    raise NonConvertableType(
-                        f'cannot convert {value_type.__name__!r} '
-                        f"to '{obj_type.__module__}.{obj_type.__name__}'"
-                    ) from None
-            
-            setattr(cls, attr, value)
-        
-        return cls.__dict__[attr]
-
-
-class Database(metaclass=YamlToClassAttr):
-    uri: str
+    uri = getenv('MONGODB_URI')
